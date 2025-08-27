a11162e24af2350a170a12f2294f17fd669d460b
Gabriel Schineider <ogabrielschneider@gmail.com>
2025-08-26 21:35:21 -0300

    First commit

---
+++ Atividade_3/exercicio3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (void)
+{
+	short int i, qntVetor = 50, temps[qntVetor], soma = 0, qntAcimaMedia = 0;
+	double media;
+	
+	printf("Catalogue %hd temperaturas.\n", qntVetor);
+	for (i=0; i < qntVetor; i++)
+	{
+		printf("Temperatura %hd: ", i+1);
+		scanf("%hd", &temps[i]);
+		soma = soma + temps[i];
+	}
+	media = soma / qntVetor;
+	
+	for (i=0; i < qntVetor; i++)
+	{
+		if (temps[i] > media)
+		{
+			qntAcimaMedia++;
+		}
+	}
+	
+	printf("Quantidade de temperaturas acima da média: %hd \n", qntAcimaMedia);
+	
+	return 0;
+}
