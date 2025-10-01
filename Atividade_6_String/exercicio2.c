3e67645302c76050a6cef430bb86b7d86d94d716
Gabriel Schineider <ogabrielschneider@gmail.com>
2025-10-01 18:28:31 -0300

    Update exercicio2.c

--- Atividade_6_String/exercicio2.c
+++ Atividade_6_String/exercicio2.c
@@ -2,12 +2,12 @@
 
 int tamanho (char* string)
 {
+	// retorna o tamanho da string sem considerar o \0
+	
 	int i=0;
-	char caracter = *string;
-	while (caracter != '\0')
+	while (string[i] != '\0')
 	{
 		i++;
-		caracter = *(string + i);
 	}
 	return i;
 }
@@ -18,11 +18,11 @@ void concatena (char* d, char* o1, char* o2)
 	int qnt_o1 = tamanho(o1);
 	int qnt_o2 = tamanho(o2);
 	
-	for (int i=0; i<=qnt_o1-1; i++)
+	for (int i=0; i < qnt_o1; i++)
 	{
 		d[i] = o1[i];
 	}
-	for (int i=0; i<=qnt_o2; i++)
+	for (int i=0; i <= qnt_o2; i++) // já add o '\0' no final
 	{
 		d[qnt_o1+i] = o2[i];
 	}
@@ -34,11 +34,11 @@ int main (void)
 	char str_origem2[] = " baby!";
 	int qnt_str_origem1 = tamanho(str_origem1);
 	int qnt_str_origem2 = tamanho(str_origem2);
-	int soma = qnt_str_origem1 + qnt_str_origem2;
-	char str_destino[soma + 1];
+	char str_destino[qnt_str_origem1 + qnt_str_origem2 + 1]; // +1 do '\0'
 	concatena(str_destino, str_origem1, str_origem2);
 		
-	printf("%s \n", str_destino);
+	printf("%s \n", str_destino); // conferindo a concatenação
+	printf("%d \n", tamanho(str_destino)); // conferindo o tamanho
 	
 	return 0;
 }
